6cf18ed0bf27aebdcf3754c8b6840250dd603285
2290774104 <2290774104@qq.com>
2025-10-28 11:23:21 +0800

    build: 修改发布目录

--- vite.config.ts
+++ vite.config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
           hook: 'closeBundle',
           targets: [
             { src: './README.md', dest: 'dist/' },
-            { src: './package.json', dest: 'dist/' },
+            // { src: './package.json', dest: 'dist/' },
           ],
         }),
       ],
